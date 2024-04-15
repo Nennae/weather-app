dbec2a706a493272bdd31210f95104c6ae2f1afa
Nennae <sezenerdal@outlook.com>
2024-04-15 15:21:56 +0200

    get city API call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,16 @@
 const key = 'K14xVSmEAdaPGKqNjJlRFMelrtnkltGH';
+
+const getCity = async (city) => {
+
+      const baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+      const query = `?apikey=${key}&q=${city}`;
+
+      const response = await fetch(baseUrl + query);
+      const data = await response.json();
+
+      return data[0];
+}
+
+getCity('stockholm')
+.then(data => console.log(data))
+.catch(error => console.log(error));
